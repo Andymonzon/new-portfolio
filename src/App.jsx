b245bd83d034414d78f56b1fa14ccebb6da1ca3b
Andymonzon <valentinomonzonandres@gmail.com>
2025-02-12 18:07:10 -0300

    add model

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { CanvasContainer } from "./components/CanvasContainer/CanvasContainer";
 
 function App() {
-  return <>hola mundo</>;
+  return (
+    <div className="w-full h-screen bg-black">
+      <CanvasContainer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/CanvasContainer/CanvasContainer.jsx
+++ src/components/CanvasContainer/CanvasContainer.jsx
@@ -1,1 +1,18 @@
-export const CanvasContainer = () => {};
+import { Canvas } from "@react-three/fiber";
+import { Room } from "../Room/Room";
+import {
+  CameraControls,
+  Environment,
+  PerspectiveCamera,
+  Sphere,
+} from "@react-three/drei";
+
+export const CanvasContainer = () => {
+  return (
+    <Canvas className="w-full h-full">
+      <Room />
+      <Environment preset="city" />
+      <CameraControls />
+    </Canvas>
+  );
+};
